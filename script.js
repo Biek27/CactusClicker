d34b09809202cf56181b6ce0ed4df854a3c705e8
Biek <81931613+Biek27@users.noreply.github.com>
2024-01-13 14:45:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function searchTable() {
 
     // Iterate through each row in the table
     for (i = 0; i < tr.length; i++) {
-        visible = false;
+        visible = true;
 
         // Iterate through each cell in the row
         for (j = 0; j < tr[i].cells.length; j++) {
@@ -22,8 +22,10 @@ function searchTable() {
 
             // If the cell content contains the filter string, make the row visible
             if (td) {
-                if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                if (td.className === 'name' && td.innerHTML.toUpperCase().indexOf(filter) > -1) {
                     visible = true;
+                } else if (td.className !== 'name' && td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    visible = false;
                 }
             }
         }
@@ -36,7 +38,7 @@ function searchTable() {
         }
     }
 }
-// Insert this function into your script.js file
+
 function generateTable(items) {
     let tbody = document.getElementById("data-table").getElementsByTagName("tbody")[0];
     let tr, td;
